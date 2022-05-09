77e4b05f24eae46cc46d01a70fe9b8a1e2e266a9
Hilary V <92323804+hvasconez24@users.noreply.github.com>
2022-05-08 22:06:49 -0500

    dotenv added

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+//import local variables
+require('dotenv').config({ path: '.env'});
+//console.log(process.env.MONGODB_URI);
+
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/budget";
+const HOST = process.env.HOST || '0.0.0.0';
 
 const app = express();
 
@@ -24,6 +29,6 @@ mongoose.connect(MONGODB_URI, {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`App running on port ${PORT}!`);
 });
